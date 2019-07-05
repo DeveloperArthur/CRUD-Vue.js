7002cfe35ba77d75084fae85838ec72dafae78c9
Arthur Santos Almeida <42553658+DeveloperArthur@users.noreply.github.com>
2019-07-05 10:59:54 -0300

    Add files via upload

--- src/components/departamento.vue
+++ src/components/departamento.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- PESQUISA -->
     <div id="pesquisa" v-if="telaPesquisa">
       <input type="text" placeholder="Digite o ID" v-model="buscarID" />
       <button class="btn btn-secondary" @click="pesquisar">Buscar</button>
@@ -17,8 +18,40 @@
         </tr>
       </table>
     </div>
+
+    <!-- EXCLUIR -->
+    <div id="excluir" v-if="telaExcluir">
+      <input type="text" placeholder="ID para excluir" v-model="excluirID" />
+      <button class="btn btn-danger" @click="excluir">Excluir</button>
+      <h3 style="color: red; font-size: 15px">
+        <strong>{{ erroExcluir }}</strong>
+      </h3>
+    </div>
+
+    <!-- ALTERAR -->
+    <div id="alterar" v-if="telaAlterar">
+      <input type="text" placeholder="ID para Alterar" v-model="alterarID" />
+      <button class="btn btn-secondary" @click="alterar">Buscar</button>
+      <h3 style="color: red; font-size: 15px">
+        <strong>{{ erroAlterar }}</strong>
+      </h3>
+      <h3>{{ departamentoAtualAlterar }}</h3>
+      <h3>{{ descricaoAtualAlterar }}</h3>
+      <hr>
+      <div>
+          <p>Digite a alteração para Departamento</p>
+          <input type="text" placeholder="Alteração departamento" v-model="novoDepartamento" />
+          <br>
+          <p>Digite a alteração para descrição</p>
+          <input type="text" placeholder="Alteração descrição" v-model="novaDescricao" />
+          <br><br>
+          <button class="btn btn-warning" @click="alterarCampos">Alterar</button>
+      </div>
+    </div>
+
+    <!-- ADICIONAR -->
     <p style="color: red" v-if="departamentos.id == 0">*Nenhum departamento cadastrado*</p>
-    <form id="formularioDepartamento" v-if="!telaPesquisa">
+    <form id="formularioDepartamento" v-if="telaAdicionar">
       <p id="nome">Departamento:</p>
       <input v-model="nomeDepartamento" placeholder="Digite o departamento" type="text" />
       <br />
@@ -29,12 +62,14 @@
       <br />
       <button class="btn btn-primary" @click="adicionar()">Adicionar</button>
     </form>
+
+    <!-- BOTOES -->
     <h3 style="color: red; font-size: 15px">
       <strong>{{ exibirErro }}</strong>
     </h3>
     <hr />
-    <button class="btn btn-danger" v-if="departamentos.id != 0">Excluir</button>
-    <button class="btn btn-secondary" v-if="departamentos.id != 0">Alterar</button>
+    <button class="btn btn-danger" v-if="departamentos.id != 0" @click="esconderExcluir">Excluir</button>
+    <button class="btn btn-secondary" v-if="departamentos.id != 0" @click="esconderAlterar">Alterar</button>
     <button class="btn btn-info" v-if="departamentos.id != 0" @click="esconderPesquisa">Pesquisar</button>
   </div>
 </template>
@@ -56,7 +91,18 @@ export default {
       pesquisaDescricao: "",
       telaPesquisa: false,
       buscarID: 0,
-      erroPesquisa: ""
+      erroPesquisa: "",
+      excluirID: 0,
+      erroExcluir: '',
+      telaExcluir: false,
+      telaAdicionar: true,
+      telaAlterar: false,
+      departamentoAtualAlterar: "",
+      descricaoAtualAlterar: "",
+      erroAlterar: "",
+      alterarID: 0,
+      novoDepartamento: "",
+      novaDescricao: ""
     };
   },
   methods: {
@@ -89,6 +135,63 @@ export default {
     },
     esconderPesquisa() {
       this.telaPesquisa = !this.telaPesquisa;
+      this.telaExcluir = false;
+      this.telaAdicionar = !this.telaAdicionar;
+      this.telaAlterar = false;
+    },
+    excluir(){
+        for (var i = 0; i < this.departamentos.id.length; i++) {
+            if (this.departamentos.id[i] == this.excluirID) {
+                this.erroExcluir = "";
+                this.departamentos.id[i] = null;
+                this.departamentos.nome[i] = null;
+                this.departamentos.descricao[i] = null;
+                alert('Departamento excluido!');
+                break;
+            }else{
+                this.erroExcluir = "Departamento nao encontrado";
+            }
+        }
+    },
+    esconderExcluir(){
+        this.telaExcluir = !this.telaExcluir;
+        this.telaPesquisa = false;
+        this.telaAdicionar = !this.telaAdicionar;
+        this.telaAlterar = false;
+    },
+    esconderAlterar(){
+        this.telaAlterar = !this.telaAlterar;
+        this.telaExcluir = false;
+        this.telaPesquisa = false;
+        this.telaAdicionar = !this.telaAdicionar;
+    },
+    alterar(){
+        for (var i = 0; i < this.departamentos.id.length; i++) {
+            if (this.departamentos.id[i] == this.alterarID) {
+                this.erroAlterar = "";
+                this.descricaoAtualAlterar = "Descrição: "+this.departamentos.descricao[i];
+                this.departamentoAtualAlterar = "Departamento: "+this.departamentos.nome[i];
+                break;
+            }else{
+                this.descricaoAtualAlterar = "";
+                this.departamentoAtualAlterar = "";
+                this.erroAlterar = "Departamento nao encontrado";
+            }
+        }
+    },
+    alterarCampos(){
+        for (var i = 0; i < this.departamentos.id.length; i++) {
+            if (this.departamentos.id[i] == this.alterarID) {
+                this.departamentos.descricao[i] = this.novaDescricao;
+                this.departamentos.nome[i] = this.novoDepartamento;
+                this.descricaoAtualAlterar = "";
+                this.departamentoAtualAlterar = "";
+                this.novaDescricao = "";
+                this.novoDepartamento = "";
+                alert('Campos alterados');
+                break;
+            }
+        }
     }
   }
 };
@@ -96,7 +199,7 @@ export default {
 
 <style>
 #formularioDepartamento,
-#pesquisa {
+#pesquisa, #excluir, #alterar {
   font-size: 20px;
 }
 table{
